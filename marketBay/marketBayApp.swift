d067cf66cccb82282e76a48defcaf7f84dec83cc
EmJhey <emjheypb@gmail.com>
2024-02-08 21:51:57 -0500

    Initial Commit

---
+++ marketBay/marketBayApp.swift
@@ -0,0 +1,17 @@
+//
+//  marketBayApp.swift
+//  marketBay
+//
+//  Created by EmJhey PB on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct marketBayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
