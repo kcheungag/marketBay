ec1c354b50f0ba523517476f65ad5b98cae40784
Dayeeta Ganguly <dayeetaganguly@yahoo.com>
2024-02-15 01:42:12 -0500

    Profile page updated

--- marketBay/Views/Authentication/ProfileView.swift
+++ marketBay/Views/Authentication/ProfileView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @EnvironmentObject var dataAccess: DataAccess
+    
+    @State private var loggedInUser: User?
+    @State private var emailFromUI : String = ""
+    @State private var nameFromUI : String = ""
+    @State private var passwordFromUI : String = ""
+    @State private var phoneNumberFromUI : String = ""
+    @State private var errorMessage : String = ""
+    
     var body: some View {
         VStack {
-            Text("PROFILE")
+            Text(self.emailFromUI)
+            TextField("Name", text: self.$nameFromUI)
+        }
+        .onAppear(){
+            loggedInUser = dataAccess.getLoggedInUser()
+            emailFromUI = loggedInUser?.email ?? ""
+            nameFromUI = loggedInUser?.name ?? ""
+            passwordFromUI = loggedInUser?.password ?? ""
+            phoneNumberFromUI = loggedInUser?.phoneNumber ?? ""
         }
     }
 }
